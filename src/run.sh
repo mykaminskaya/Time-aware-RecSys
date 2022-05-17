5e7b9a970eac55c92b73b533b492901ffaea9856
mykaminskaya <57842792+mykaminskaya@users.noreply.github.com>
2022-05-17 17:24:31 +0300

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -x
 
+python main.py --model_name GRU4Rec --emb_size 64 --hidden_size 100 --lr 1e-3 --l2 1e-4 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
 
-python3.7 main.py --model_name SASRec --metric 'NDCG,HR,MNAP' --topk '1,5,10,15,20' --emb_size 64 --epoch 200 --num_layers 1 --num_heads 1 --lr 1e-4 --l2 1e-6 --history_max 20 --dataset 'Grocery_and_Gourmet_Food' --log_file '../../m.y.kaminskaya/ReChoruc/log/version_1.txt'
+python main.py --model_name SASRec --emb_size 64 --num_layers 1 --num_heads 1 --lr 1e-4 --l2 1e-6 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
+
+python main.py --model_name TiSASRec --emb_size 64 --num_layers 1 --num_heads 1 --lr 1e-4 --l2 1e-6 --history_max 20 --dataset 'Grocery_and_Gourmet_Food'
